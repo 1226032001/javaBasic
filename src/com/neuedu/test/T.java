ac0a67e103cffa496c5a9ed1f48e14ab6ac63437
1226032001 <1226032001@qq.com>
2019-05-23 20:09:08 +0800

    ccc

--- src/com/neuedu/test/T.java
+++ src/com/neuedu/test/T.java
@@ -1,7 +1,21 @@
 package com.neuedu.test;
-
+import java.util.Scanner;//要使用scanner这个类，就需要导入一个输入接口相关的包
 public class T {
     public static void main(String[] args) {
-        System.out.println(1);
-    }
+        /*int a =654;
+        int b = 0;
+        while (a !=0){
+            b = a%10;
+            System.out.print(b);
+            a = a/10;
+        }
+*/
+        int a = 186;
+        int b = a%10;
+        int c =a/10%10;
+        int d= a/100%10;
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        }
 }
