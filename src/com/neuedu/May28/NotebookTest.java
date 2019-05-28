18a42e88e18ce73d27052a48a4f08812400d9056
1226032001 <1226032001@qq.com>
2019-05-28 22:42:26 +0800

    ccc

---
+++ src/com/neuedu/May28/NotebookTest.java
@@ -0,0 +1,10 @@
+package com.neuedu.May28;
+
+public class NotebookTest {
+    public static void main(String[] args) {
+        Notebook notebook1 = new Notebook();
+        System.out.println(notebook1);
+        Notebook notebook2 = new Notebook('红', 007);
+        System.out.println(notebook2);
+    }
+    }
---
+++ src/com/neuedu/May28/StudentText.java
@@ -0,0 +1,14 @@
+package com.neuedu.May28;
+
+
+
+public class StudentText {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.Run();
+
+
+    }
+
+    }
